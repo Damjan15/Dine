2c8a83cba70b512cc5cee8d9b2f3de4086d9d352
Damjan15 <stamenovdamjan20@gmail.com>
2023-01-17 23:26:02 +0100

    head tags added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
-import { Booking, Enjoy, Gathering, Hero, Highlights, LocallySourced } from "@/components";
+import Head from "next/head";
+import {
+  Booking,
+  Enjoy,
+  Gathering,
+  Hero,
+  Highlights,
+  LocallySourced,
+} from "@/components";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Dine | Exquisite dining since 1989</title>
+        <meta
+          name="description"
+          content="Experience the delicious tastes of Dine, the premier restaurant established in 1989. Discover our menu, location, and hours of operation on our website."
+        />
+        <meta
+          name="keywords"
+          content="restaurant, Dine, established, 1989, menu, location, hours, operation, delicious, tastes, premier"
+        />
+      </Head>
+
       <Hero />
       <Enjoy />
       <LocallySourced />
@@ -10,5 +30,5 @@ export default function Home() {
       <Gathering />
       <Booking />
     </>
-  )
+  );
 }
--- pages/reservations.js
+++ pages/reservations.js
@@ -1,8 +1,21 @@
+import Head from "next/head";
 import { ReservationSection, ReservationsHero } from "@/components";
 
 export default function Reservations() {
   return (
     <>
+      <Head>
+        <title>Reservations | Dine 1989</title>
+        <meta
+          name="description"
+          content="Experience the delicious tastes of Dine, the premier restaurant established in 1989. Discover our menu, location, and hours of operation on our website."
+        />
+        <meta
+          name="keywords"
+          content="restaurant, Dine, established, 1989, menu, location, hours, operation, delicious, tastes, premier"
+        />
+      </Head>
+
       <ReservationsHero />
       <ReservationSection />
     </>
